86f10cd5d42ec048689d37e8f2d573c549ba2f36
akshayLuhadia <akshayLuhadia@github.com>
2020-02-23 15:19:15 +0530

    added build files

---
+++ precache-manifest.163e9fdb07c43acb1ca299afea7811fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ee978063a4a512a64197b37f9997f8",
+    "url": "/DemoPages/index.html"
+  },
+  {
+    "revision": "964e9304a3b487f4a474",
+    "url": "/DemoPages/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "9b3f6063591b507eed94",
+    "url": "/DemoPages/static/js/2.dc845d7d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/DemoPages/static/js/2.dc845d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964e9304a3b487f4a474",
+    "url": "/DemoPages/static/js/main.65f34551.chunk.js"
+  },
+  {
+    "revision": "5b1b4174fa123d101655",
+    "url": "/DemoPages/static/js/runtime-main.5eb2ee2e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/DemoPages/static/media/logo.ee7cd8ed.svg"
+  }
+]);
