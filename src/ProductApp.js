a7053fa01b624bac18a3f157f83bff3abf69871b
Akshay Luhadia <akshayluhadia.nmims@gmail.com>
2020-05-30 15:47:06 +0530

    revert code

--- src/ProductApp.js
+++ src/ProductApp.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ProductList from './components/ProductList';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import EditProduct from './components/EditProduct';
+import React from "react";
+import ProductList from "./components/ProductList";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import EditProduct from "./components/EditProduct";
 
 export default function ProductApp() {
-    return (
-        <div className="product-app">
-            <Router>
-                <div>
-                    <Route exact path="/" component={ProductList} />
-                    <Route path="/edit-product/:id" component={EditProduct} />
-                </div>
-            </Router>
+  return (
+    <div className="product-app">
+      <Router>
+        <div>
+          <Route exact path="/DemoPages" component={ProductList} />
+          <Route path="/DemoPages/edit-product/:id" component={EditProduct} />
         </div>
-    )
+      </Router>
+    </div>
+  );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => (
-    <tr>
-        <td>{product.name}</td>
-        <td>{product.weight}</td>
-        <td>{product.availability}</td>
-        <td>
-            {product.isEditable ?
-                <Link to={`/edit-product/${product.id}`}>
-                    <button>EDIT</button>
-                </Link>
-                : ""}
-        </td>
-    </tr>
+  <tr>
+    <td>{product.name}</td>
+    <td>{product.weight}</td>
+    <td>{product.availability}</td>
+    <td>
+      {product.isEditable ? (
+        <Link to={`/DemoPages/edit-product/${product.id}`}>
+          <button>EDIT</button>
+        </Link>
+      ) : (
+        ""
+      )}
+    </td>
+  </tr>
 );
 
 export default connect()(Product);
